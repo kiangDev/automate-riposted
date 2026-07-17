650311436f9103083f77fe1c1bec452d23b74bd0
kiang <a.akarawat149@gmail.com>
2026-07-17 16:23:14 +0700

    fix-csv

--- automate_script.py
+++ automate_script.py
@@ -828,7 +828,7 @@ def main():
                             auto_id=SHIPPING_SERVICE_AUTO_ID,
                             control_type="Button",
                             wait_states="exists visible",
-                            timeout=8,
+                            timeout=10,
                         )
 
                         # แก้: ช่วงนี้มีโอกาสเจอหน้า "ข้อมูลผู้ส่ง" (customer
